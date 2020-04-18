2491398aea52a89563374535aa9392cd786429a2
WraithOne <wraithone@outlook.de>
2020-04-18 13:08:10 +0200

    added basic movement

--- main/Player/Player.go
+++ main/Player/Player.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/Player/Arzt.atlas\"\n"
-  "default_animation: \"Arzt\"\n"
+  "default_animation: \"Idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
