42a5caac461dd51b427e6290bf470d9eb239d86c
Gorbert <Dan.urb@web.de>
2020-04-18 15:18:19 +0200

    kollision V2

--- main/Player/Player.go
+++ main/Player/Player.go
@@ -23,7 +23,7 @@ embedded_components {
   ""
   position {
     x: 0.0
-    y: -1.0
+    y: -0.0
     z: 0.0
   }
   rotation {
@@ -36,8 +36,8 @@ embedded_components {
 embedded_components {
   id: "Arzt"
   type: "collisionobject"
-  data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  data: "collision_shape: \"/main/Map/hospital1.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
@@ -60,11 +60,11 @@ embedded_components {
   "    index: 0\n"
   "    count: 1\n"
   "  }\n"
-  "  data: 14.592\n"
+  "  data: 13.967\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
+  "locked_rotation: true\n"
   ""
   position {
     x: 0.0
