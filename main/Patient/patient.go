731c4218e09b5e3955da29fca3c349a907f80f35
WraithOne <wraithone@outlook.de>
2020-04-19 20:01:46 +0200

    Added patient and ingame ui

---
+++ main/Patient/patient.go
@@ -0,0 +1,30 @@
+components {
+  id: "patient"
+  component: "/main/Patient/patient.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "patient1"
+  component: "/main/Patient/patient.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
