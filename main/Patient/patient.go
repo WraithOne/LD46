3a94e93a221cff1258067c5cd21f7e032c409a1d
WraithOne <wraithone@outlook.de>
2020-04-20 21:19:08 +0200

    changed patient animation

--- main/Patient/patient.go
+++ main/Patient/patient.go
@@ -32,7 +32,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/Patient/patient.atlas\"\n"
-  "default_animation: \"combi2_vert\"\n"
+  "default_animation: \"tshirtgrbl\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
