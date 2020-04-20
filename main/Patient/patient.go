82f1cd9913c063ecf6a98d703bae146162887743
WraithOne <wraithone@outlook.de>
2020-04-20 23:19:44 +0200

    updated patient spawn

--- main/Patient/patient.go
+++ main/Patient/patient.go
@@ -13,21 +13,6 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "patient1"
-  component: "/main/Patient/patient.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
